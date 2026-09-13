8e82a819d361d5f2e059dbb217f72720cfce535a
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2026-09-13 09:41:47 -0300

    chore: teste conexão

--- src/dbconfig.js
+++ src/dbconfig.js
@@ -2,29 +2,30 @@
  * Configurações e conexão com o banco de dados.
  */
 
-// Importa a biblioteca dotenv, responsável por carregar
-// as variáveis de ambiente armazenadas no arquivo .env
-const dotenv = require('dotenv');
-
-// Carrega as variáveis do arquivo .env para o objeto process.env
-dotenv.config();
-
-// Essa função permite estabelecer a conexão com um banco
-// de dados PostgreSQL hospedado no Neon.
-const { neon } = require('@neondatabase/serverless');
-
 /**
  * Cria uma conexão com o banco de dados.
  * @returns 
  */
-async function createDbConnection() {
+function createDbConnection() {
+
+  // Importa a biblioteca dotenv, responsável por carregar
+  // as variáveis de ambiente armazenadas no arquivo .env
+  const dotenv = require('dotenv');
+
+  // Carrega as variáveis do arquivo .env para o objeto process.env
+  dotenv.config();
+
+  // Essa função permite estabelecer a conexão com um banco
+  // de dados PostgreSQL hospedado no Neon.
+  const { neon } = require('@neondatabase/serverless');
 
   // Cria a conexão com o banco de dados utilizando a string
   // de conexão armazenada na variável de ambiente DATABASE_URL.
   const sql = neon(process.env.DATABASE_URL);
-
+    
   console.log("Conexão com PostgreSQL foi estabelecida");
 
+  //Cria a tabela do banco de dados.
   createTable(sql);  
 
   return sql;
@@ -37,7 +38,7 @@ async function createDbConnection() {
  */
 async function createTable(db) {
      try {
-      await db`CREATE TABLE cliente (
+      await db`CREATE TABLE IF NOT EXISTS cliente (
             clienteId INTEGER, 
             nome VARCHAR(100), 
             cpf VARCHAR(11), 
@@ -49,5 +50,4 @@ async function createTable(db) {
   }
 
 // Inicializa a conexão e criação da tabela
-
-module.exports = sql;
+module.exports = createDbConnection();
